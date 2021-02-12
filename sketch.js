107e65a18b7d123a916b99f66879e250091d37dc
Ang Jun Ray <angjunray@live.com>
2021-02-12 19:03:09 +0800

    Eraser added + a lot of html improvements yay

--- sketch.js
+++ sketch.js
@@ -2,12 +2,15 @@
 Defining variables/Setup
 */
 let colour = "black", thickness = 4, tool = "Pen", opacity = 50; //defining the input values
-let drawingPoints = [], inputValues = ['colour', 'thickness', 'Tool', 'opacity'], drawn = [], colourSettings = [];
+let drawingPoints = [], inputValues = ['Colour', 'Thickness', 'Tool', 'Opacity'], drawn = [], colourSettings = [], toBeRun = [];
+let eraserReq = [['Thickness', '1', 'number'], ['Opacity', "255", "number"]], penReq = [['Colour', "#000000", "color"], ['Thickness', "1", "number"], ['Opacity', "255", "number"]];
 function setup() {
+    noFill();
     createCanvas(500, 500);
     rectMode(CENTER);
     frameRate(100);
     background(0, 0);
+    changeTool("Pen");
 }
 
 /*
@@ -15,15 +18,18 @@ HTML-Javascript Inputs
 */
 
 function getInput(input) {
-    if (input == 'colour') {
+    if (input == 'Colour') {
         colour = document.getElementById(inputValues[0]).value;
-    } else if (input == 'thickness') {
+    } else if (input == 'Thickness') {
         thickness = document.getElementById(inputValues[1]).value;
     } else if (input == 'ResetCanvas') {
-        clear();
+        drawn = [];
+        fill(255);
+        rect(width / 2, height / 2, width, height);
     } else if (input == 'Tool') {
         tool = document.getElementById(inputValues[2]).value;
-    } else if (input == 'opacity') {
+        changeTool(tool);
+    } else if (input == 'Opacity') {
         opacity = document.getElementById(inputValues[3]).value;
     }
 }
@@ -40,7 +46,11 @@ function reloadDrawn() {
         stroke(colourToUse);
         counter++;
         for (const j of i) {
-            vertex(j.x, j.y);
+            if (j[1] == "Eraser") {
+                stroke("white");
+                noFill();
+            }
+            vertex(j[0].x, j[0].y);
         }
         endShape();
     }
@@ -51,6 +61,7 @@ function reloadDrawn() {
 Own Functions
 */
 function defaultSettings() {
+    noFill();
     colour = color(colour);
     colour.setAlpha(opacity);
     stroke(colour);
@@ -58,20 +69,20 @@ function defaultSettings() {
 }
 
 function LoadChanges() {
-    for (const i of inputValues) {
+    for (const i of toBeRun) {
         getInput(i);
     }
 }
 
 function canvasBorder() {
-    fill(255);
+    fill(255, 255, 255, 0);
     rect(width / 2, height / 2, width, height);
 }
 /*
 P5 Functions
 */
 function mouseDragged() {
-    drawingPoints.push(createVector(mouseX, mouseY));
+    drawingPoints.push([createVector(mouseX, mouseY), tool]);
 }
 
 function mouseReleased() {
@@ -83,27 +94,96 @@ function mouseReleased() {
 /*
 Tools
 */
+function changeTool(Tool) {
+    console.log(Tool);
+    const myNode = document.getElementById("Tool Selection");
+    while (myNode.firstChild) {
+        myNode.removeChild(myNode.lastChild);
+        
+    }
+    if (Tool == "Pen") {
+        toBeRun = [];
+        for (const i of penReq) {
+            toBeRun.push(i[0]);
+            //label
+            let parentDiv = document.getElementById("Tool Selection");
+            let element = document.createElement("label");
+            element.setAttribute("for", i[0]);
+            element.innerHTML = i[0];
+            parentDiv.appendChild(element);
+            //Input
+            let inputElement = document.createElement("input");
+            inputElement.setAttribute("type", i[2]);
+            inputElement.setAttribute("id", i[0]);
+            inputElement.setAttribute("value", i[1]);
+            inputElement.setAttribute("onclick", "getInput(" + i[0] + ")");
+            parentDiv.appendChild(inputElement);
+            //backspace
+            let backSpace = document.createElement("br");
+            parentDiv.appendChild(backSpace);
+            let backSpace1 = document.createElement("br");
+            parentDiv.appendChild(backSpace1);
+        }
+    } else if (Tool == "Eraser") {
+        toBeRun = [];
+        for (const i of eraserReq) {
+            console.log(i);
+            toBeRun.push(i[0]);
+            //label
+            let parentDiv = document.getElementById("Tool Selection");
+            let element = document.createElement("label");
+            element.setAttribute("for", i[0]);
+            element.innerHTML = i[0];
+            parentDiv.appendChild(element);
+            //Input
+            let inputElement = document.createElement("input");
+            inputElement.setAttribute("type", i[2]);
+            inputElement.setAttribute("id", i[0]);
+            inputElement.setAttribute("value", i[1]);
+            inputElement.setAttribute("onclick", "getInput(" + i[0] + ")");
+            parentDiv.appendChild(inputElement);
+            //backspace
+            let backSpace = document.createElement("br");
+            parentDiv.appendChild(backSpace);
+            let backSpace1 = document.createElement("br");
+            parentDiv.appendChild(backSpace1);
+        }
+    }
+}
+
 function Pen() {
     push();
     defaultSettings();
-    noFill();
     beginShape();
     for (const line of drawingPoints) {
-        vertex(line.x, line.y);
+        vertex(line[0].x, line[0].y);
     }
     endShape();
     pop();
 }
 
+function Eraser() {
+    push();
+    defaultSettings();
+    stroke("white");
+    beginShape();
+    for (const line of drawingPoints) {
+        vertex(line[0].x, line[0].y);
+    }
+    endShape();
+    pop();
+}
 /*
 Runtime
 */
 function draw() {
     strokeWeight(20);
-    canvasBorder();
     reloadDrawn();
     if (tool == "Pen") {
         Pen();
+    } else if (tool == "Eraser") {
+        Eraser();
     }
     LoadChanges();
+    canvasBorder();
 }
